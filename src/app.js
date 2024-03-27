eb6f3f4977e8b2824ab75afaa2ef7a5382bdcd79
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-03-27 19:34:53 +0000

    primero

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["Arroz", "Pasta"];
+  let adj = ["pollo", "gambas"];
+  let noun = ["cebolla", "curry"];
+
+  let domGen = '<ul class="list-group">';
+  domGen +=
+    '<li class="list-group-item bg-pink text-light fw-bolder">Menú de la semana</li>';
+
+  pronoun.map(iterator => {
+    adj.map(item => {
+      noun.map(element => {
+        domGen += `
+        <li class="list-group-item">
+        ${iterator} con ${item} y ${element}
+        </li>
+        `;
+      });
+    });
+  });
+  domGen += "</ul>";
+  document.querySelector("#randomGen").innerHTML = domGen;
 };
